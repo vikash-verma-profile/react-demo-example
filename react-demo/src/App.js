74276a51cc85159ce33a09c37b82b4bde3231f97
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-10-26 11:21:08 +0530

    first commit

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -10,11 +10,11 @@ function App() {
   const expenses=[
     { title:'New Tv',amount:799.49,date:new Date(2021,2,12)},
     { title:'Car Insurance',amount:294.34, date:new Date(2021,2,20)}
-  ]
+  ];
   return (
    <div>
-     <ExpenseItem title="New Tv" amount="799.49" date="new Date(2021,2,12)" />
-     <ExpenseItem  title="New Tv" amount="799.49" date="new Date(2021,2,12)" />
+     <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+     <ExpenseItem  title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}  />
      </div>
   );
 }
--- react-demo/src/components/expenseItem.js
+++ react-demo/src/components/expenseItem.js
@@ -1,11 +1,9 @@
 import  '../css/ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate=new Date(2021,10,26);
-    const expenseTitle='Car Insurance';
-    const expenseAmount=294.67;
+   
     return ( <div className="expense-item">
-        <div> {props.date }</div> 
+        <div>{props.date.toDateString() }</div> 
         <div className="expense-item__description">
         <h2> {props.title}</h2> 
         </div> <div className="expense-item__price"> {props.amount} </div> </div >
