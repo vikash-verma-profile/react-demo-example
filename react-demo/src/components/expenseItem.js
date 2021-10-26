af12b487361243e32a306e9f6cfb62d56c208ee3
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-10-26 11:26:42 +0530

    Date

--- react-demo/src/components/expenseItem.js
+++ react-demo/src/components/expenseItem.js
@@ -2,8 +2,15 @@ import  '../css/ExpenseItem.css';
 
 function ExpenseItem(props) {
    
+    const month=props.date.toLocaleString('en-Us',{month:'long'});
+    const day=props.date.toLocaleString('en-Us',{day:'2-digit'});
+    const year=props.date.getFullYear();
     return ( <div className="expense-item">
-        <div>{props.date.toDateString() }</div> 
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+            </div> 
         <div className="expense-item__description">
         <h2> {props.title}</h2> 
         </div> <div className="expense-item__price"> {props.amount} </div> </div >
